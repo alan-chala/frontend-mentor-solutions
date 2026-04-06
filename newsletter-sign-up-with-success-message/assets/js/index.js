d2367db2c2b38bc8726be7d8cf902551cee6665e
alan-chala <alan.chala000@gmail.com>
2026-04-06 11:43:01 -0500

    feat(newsletter): enhance form functionality with success message and error handling

--- newsletter-sign-up-with-success-message/assets/js/index.js
+++ newsletter-sign-up-with-success-message/assets/js/index.js
@@ -1,26 +1,35 @@
+const signUpFormElement = document.querySelector(".sign-up-form");
 const formElement = document.querySelector("form");
 const inputElement = document.querySelector("input");
 const errorMessageElement = document.querySelector(".error-message");
+const successMessageElement = document.querySelector(".success-message");
+const userEmailElement = document.querySelector(".user-email");
+const dimissButtonElement = document.querySelector(".dimiss-button");
 
 formElement.addEventListener("submit", (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
-  const isValidEmail = validarEmail(inputElement.value)
+  const isValidEmail = validarEmail(inputElement.value);
 
   if (!isValidEmail || inputElement.value === "") {
-    inputElement.classList.add("input-error")
-    errorMessageElement.style.display = "block"
-    console.log("Email inválido")
+    inputElement.classList.add("input-error");
+    errorMessageElement.style.display = "block";
+  } else {
+    inputElement.classList.remove("input-error");
+    errorMessageElement.style.display = "none";
+    signUpFormElement.classList.add("sign-up-hidden");
+    successMessageElement.classList.add("active");
+    userEmailElement.textContent = inputElement.value;
   }
-  else {
-    inputElement.classList.remove("input-error")
-    errorMessageElement.style.display = "none"
-    console.log("Email válido")
-  }
+});
 
-})
+dimissButtonElement.addEventListener("click", () => {
+  successMessageElement.classList.remove("active");
+  signUpFormElement.classList.remove("sign-up-hidden");
+  inputElement.value = "";
+});
 
 function validarEmail(email) {
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return regex.test(email);
-}   
+}
