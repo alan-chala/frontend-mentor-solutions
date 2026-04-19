ca730563cebf3d6adb2c965401e92c301cafd7ab
alan-chala <alan.chala000@gmail.com>
2026-04-19 12:49:09 -0500

    feat(landing): add navigation bar and enhance animations with GSAP

--- landing-project/src/js/main.js
+++ landing-project/src/js/main.js
@@ -1,1 +1,32 @@
-console.log("Hello, world!")
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
+
+const split = new SplitText(".split-text", {
+  type: "chars",
+});
+
+gsap.from("h1", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  ease: "power4.out",
+  stagger: 0.1,
+})
+
+gsap.from("header p", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  ease: "power4.out",
+  delay: 0.5,
+})
+
+gsap.from("#view-solutions-link", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  ease: "power4.out",
+  delay: 1,
+})
