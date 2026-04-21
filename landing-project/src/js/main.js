f856c915961c69f96a2cd24f10643ebc4b32d740
alan-chala <alan.chala000@gmail.com>
2026-04-20 21:55:50 -0500

    feat(landing): enhance layout and animations with improved styles and GSAP transitions

--- landing-project/src/js/main.js
+++ landing-project/src/js/main.js
@@ -3,30 +3,41 @@ import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(SplitText);
 
-const split = new SplitText(".split-text", {
-  type: "chars",
+const h1Split = new SplitText("h1", { type: "lines" });
+const paragraphSplit = new SplitText("header p", { type: "lines" });
+
+const tl = gsap.timeline({
+  defaults: {
+    opacity: 0,
+    ease: "power3.out",
+    duration: 1,
+  },
 });
 
-gsap.from("h1", {
-  y: 100,
-  opacity: 0,
-  duration: 2,
-  ease: "power4.out",
-  stagger: 0.1,
+tl.from("nav span", {
+  x: -50,
+  duration: 0.8,
 })
-
-gsap.from("header p", {
-  y: 100,
-  opacity: 0,
-  duration: 2,
-  ease: "power4.out",
-  delay: 0.5,
-})
-
-gsap.from("#view-solutions-link", {
-  y: 100,
-  opacity: 0,
-  duration: 2,
-  ease: "power4.out",
-  delay: 1,
-})
+  .from(
+    h1Split.lines,
+    {
+      y: 50,
+      stagger: 0.1,
+    },
+    "-=0.4",
+  )
+  .from(
+    paragraphSplit.lines,
+    {
+      y: 30,
+      stagger: 0.05,
+    },
+    "-=0.6",
+  )
+  .from(
+    "#view-solutions-link",
+    {
+      y: 20,
+    },
+    "-=0.8",
+  );
